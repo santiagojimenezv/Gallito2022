50cbe4cba09bedd0209642cb540e805199889c3f
Santiago <s.jimenezv10@gmail.com>
2022-03-01 17:33:31 -0500

    Se agrego paginacion a los posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,20 +4,23 @@
 
 <div class="container">
     <h1>Publicaciones de {{ $user->name}}</h1>
-@forelse($posts as $post)
-    <div class="card mb-2" >
-        <div class="card-body">
-            <h5 class="card-title">{{ $user->name }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at->format('d/m/Y')}}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at->diffForHumans()}}</h6>
-            <p class="card-text">{{ $post->content }}</p>
-        </div>
+        @forelse($posts as $post)
+            <div class="card mb-2" >
+                <div class="card-body">
+                    <h5 class="card-title">{{ $user->name }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at->format('d/m/Y')}}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at->diffForHumans()}}</h6>
+                    <p class="card-text">{{ $post->content }}</p>
+                </div>
+            </div>
+        @empty
+            <div class="alert alert-info" role="alert">
+                El usuario no ha publicado mensajes
+            </div>
+        @endforelse
+    <div class="mt-3">
+        {{$posts->links()}}
     </div>
-@empty
-    <div class="alert alert-info" role="alert">
-        El usuario no ha publicado mensajes
-    </div>
-@endforelse
 </div>
 
 @endsection
